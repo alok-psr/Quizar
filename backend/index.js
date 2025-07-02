f4a3ef7986be73875c1c171f8838f8090d4d8ee9
alok <alokrathore136@gmail.com>
2025-07-02 19:25:45 +0530

    proxy frontend ready --> the frontend part works .. without actual questions part and rankings and multiplayer thing .. need to add backend

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,21 @@ app.use(express.json())
 
 const PORT = 3000
 
+const roomData=[
+    {
+        rn:123,
+        players:['a','b','c']
+    }
+]
+    
+
 app.listen(PORT)
 
 app.get('/',(req,res)=>{
     res.json({msg:'Running !!!',name:'alok'})
+})
+
+app.get('/room/:rn',(req,res)=>{
+    // rn is the room number and the people coming at this will be able to join the room
+    // we create an object 
 })
