840e148768ed80952ec499021767ec23e3aa802f
alok <alokrathore136@gmail.com>
2025-07-13 09:38:14 +0530

    updated stuff

--- frontend/src/sockets/socket.js
+++ frontend/src/sockets/socket.js
@@ -1,8 +1,6 @@
 import {io} from 'socket.io-client'
-import dotenv from 'dotenv'
-dotenv.config({path:'../.env'})
 
-const backend_server = process.env.BACKEND_SERVER
+const backend_server = 'https://quizar.onrender.com'
 
 const socket = io(backend_server,{
     autoConnect:false,
